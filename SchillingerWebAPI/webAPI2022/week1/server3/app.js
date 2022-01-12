f9eef899f19045b93231a5d15e147d587850adab
ImagineGavins <97621718+ImagineGavins@users.noreply.github.com>
2022-01-12 13:20:38 -0500

    Add files via upload

---
+++ SchillingerWebAPI/webAPI2022/week1/server3/app.js
@@ -0,0 +1,32 @@
+var http = require("http")
+var url = require("url")
+var port = 3000
+var fileSystem = require('fs')
+
+http.createServer(function(request, response)
+{
+    var pathName = request.url
+    var fileName = "index.html"
+
+    // Load file using the filesystem
+    fileSystem.readFile(fileName, callback);
+
+    function callback(error, data)
+    {
+        if(error)
+        {
+            console.log(error)
+            response.writeHead(200, {"Content-type":"text/html"})
+            response.write("<!DOCTYPE><html><body><div> Page not found </div></body></html>")
+        }
+        else
+        {
+            response.writeHead(200, {"Content-type":"text/html"})
+            response.write(data)
+        }
+    }
+
+    response.end()
+}).listen(port)
+
+console.log("Running on port 3000")
