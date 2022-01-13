1f11c23a3bea0ef96f6b5faf0f6bdaaffafe79f4
ImagineGavins <imaginegavins@gmail.com>
2022-01-13 14:01:30 -0500

    Commit 2

--- SchillingerWebAPI/webAPI2022/week1/StaticSite/app.js
+++ SchillingerWebAPI/webAPI2022/week1/StaticSite/app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const port = process.env.port||3000
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname+"/views"))
 
 app.get('/', (request, response)=>{
     response.sendFile(__dirname+"/views/index.html")
