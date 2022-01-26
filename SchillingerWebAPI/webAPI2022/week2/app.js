b20f98c514a802b610fd184d3320670d75572e1d
ImagineGavins <imaginegavins@gmail.com>
2022-01-26 14:55:23 -0500

    kjhg;sdfhgfd

--- SchillingerWebAPI/webAPI2022/week2/app.js
+++ SchillingerWebAPI/webAPI2022/week2/app.js
@@ -6,11 +6,15 @@ var mongoose = require("mongoose")
 
 // basic code for saving an entry
 // model of data
-var Game = mongoose.model('Game', {nameofGame:String})
-var game = new Game({nameofGame:"Skyrim"})
-game.save().then(function(){
-    console.log("Game Saved")
-})
+//var Game = mongoose.model('Game', {nameofGame:String})
+//var game = new Game({nameofGame:"Skyrim"})
+//game.save().then(function(){
+//    console.log("Game Saved")
+//})
+
+// Use Models for Database
+require('./models/Game')
+var Game = mongoose.model('game')
 
 // setup connection to mongoose
 mongoose.connect('mongodb://localhost:27017/gameEntries',{
@@ -19,6 +23,15 @@ mongoose.connect('mongodb://localhost:27017/gameEntries',{
     console.log("Connected to MongoDB Database")
 }).catch(function(err){
     console.log(err)
+})
+
+app.post('/saveGame', function(req,res){
+    console.log("Request Made")
+    console.log(req.body);
+    // Save
+    new Game(req.body).save().then(function(){
+        res.redirect('gamelist.html')
+    })
 })
 
 app.use(express.static(__dirname+"/views"))
